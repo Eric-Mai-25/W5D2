67484cb9d44e9a15d7d4dd0d235f740d596fbe9f
Eric-Mai-25 <ericmai2@gmail.com>
2023-08-15 15:40:08 -0700

    Sql 07 finished

--- aa-sql-sql-zoo-main/lib/07_joins_exercises.rb
+++ aa-sql-sql-zoo-main/lib/07_joins_exercises.rb
@@ -122,6 +122,16 @@ def prolific_actors
   # Obtain a list in alphabetical order of actors who've had at least 15
   # starring roles.
   execute(<<-SQL)
+    SELECT actors.name
+    FROM actors
+    JOIN castings
+    ON castings.actor_id = actors.id
+    JOIN movies
+    ON movies.id = castings.movie_id
+    WHERE castings.ord = 1
+    GROUP BY actors.name
+    HAVING COUNT(movies.title) >= 15
+    ORDER BY actors.name; 
   SQL
 end
 
@@ -129,11 +139,37 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
+    SELECT movies.title, COUNT(*)
+    FROM movies
+    JOIN castings
+    ON castings.movie_id = movies.id
+    JOIN actors
+    ON castings.actor_id = actors.id
+    WHERE movies.yr = 1978
+    GROUP BY movies.title
+    ORDER BY COUNT(*) DESC , movies.title;
   SQL
 end
 
 def colleagues_of_garfunkel
   # List all the people who have played alongside 'Art Garfunkel'.
   execute(<<-SQL)
+    SELECT actors.name
+    FROM actors
+    JOIN castings
+    ON castings.actor_id = actors.id
+    JOIN (
+      SELECT *
+      FROM movies
+      JOIN castings
+      ON castings.movie_id = movies.id
+      JOIN actors
+      ON actors.id = castings.actor_id
+      WHERE actors.name = 'Art Garfunkel'
+    ) as movie_filtered
+    ON movie_filtered.movie_id = castings.movie_id
+    WHERE actors.name != 'Art Garfunkel';
+
+
   SQL
 end
